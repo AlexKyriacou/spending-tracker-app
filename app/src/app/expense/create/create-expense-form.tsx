1ef2ddc53f212de491f9c0f15d49896da994bd19
Alex Kyriacou <a.kyriacou14@gmail.com>
2024-02-07 15:37:25 +1100

    fix: make default date valid

--- app/src/app/expense/create/create-expense-form.tsx
+++ app/src/app/expense/create/create-expense-form.tsx
@@ -179,7 +179,7 @@ export default function CreateExpenseForm() {
                   <Calendar
                     mode="single"
                     selected={field.value}
-                    onSelect={field.onChange}
+                    onSelect={(date) => field.onChange(date)}
                     disabled={(date) => date < new Date("1900-01-01")}
                     initialFocus
                   />
